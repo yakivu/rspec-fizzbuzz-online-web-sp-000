b9129077fc473c44365d631c8e65fd0a0f8069bf
Adam Walker <adamw207@yahoo.com>
2020-04-08 18:33:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,21 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do This
-require_relative './spec_helper.rb'
- 
+#require_relative './spec_helper.rb'
+
+#require_relative '../fizzbuzz' 
+
+def fizzbuzz(num)
+  if (num % 5 ==0) && (num % 3 == 0)
+    return "FizzBuzz"
+    elsif num % 3 == 0 
+    return "Fizz"
+    elsif num % 5 == 0 
+    return "Buzz"
+  else
+    return
+  end
+end
+
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
